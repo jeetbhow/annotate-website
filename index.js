c3eb6b5a3f616c0fa2d0b62e51099c76b8c8313a
Jeet Bhowmik <jeetbhowm@gmail.com>
2024-05-12 22:58:56 -0700

    Finished Hero and Features section on mobile

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
+const tabs = document.getElementsByClassName("tab");
+const panels = document.getElementsByClassName("panel");
 const hamburger = document.getElementById("hamburger");
 const nav = document.querySelector("nav");
 
 hamburger.addEventListener("click", handleHamburgerClick);
+for (const tab of tabs) {
+  tab.addEventListener("click", handleTabClick);
+}
+
+/**
+ * Event Handlers
+ */
 
 function handleHamburgerClick() {
   hamburger.classList.toggle("open");
   nav.classList.toggle("hidden");
+}
+
+function handleTabClick(e) {
+  const targetKey = e.target.getAttribute("key"); // The key for the tab you clicked on.
+
+  // Apple the styling to the selected tab.
+  for (const tab of tabs) {
+    if (tab.hasAttribute("active")) {
+      tab.toggleAttribute("active");
+    }
+    if (tab.getAttribute("key") === targetKey) {
+      tab.toggleAttribute("active");
+    }
+  }
+
+  // Switch to the correct panel
+  for (const panel of panels) {
+    if (panel.getAttribute("key") === targetKey) {
+      panel.classList.remove("hidden");
+    } else {
+      panel.classList.add("hidden");
+    }
+  }
 }
