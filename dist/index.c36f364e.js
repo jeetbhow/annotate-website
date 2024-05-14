80560b75333dc73ddefef4291b24249a84d104ac
Jeet Bhowmik <jeetbhowm@gmail.com>
2024-05-14 03:44:16 -0700

    fix: Fixed some formatting issues
    Fixed the spacing between the heading and text on the hero and features page

---
+++ dist/index.c36f364e.js
@@ -0,0 +1,41 @@
+const tabs = document.getElementsByClassName("tab");
+const panels = document.getElementsByClassName("panel");
+const accordionHeaders = document.getElementsByClassName("accordion-header");
+const hamburger = document.getElementById("hamburger");
+const nav = document.querySelector("nav");
+hamburger.addEventListener("click", handleHamburgerClick);
+for (const tab of tabs)tab.addEventListener("click", handleTabClick);
+for (const header of accordionHeaders)header.addEventListener("click", handleAccordionClick);
+/**
+ * Event Handlers
+ */ function handleAccordionClick(e) {
+    // Set all accordion items to inactive.
+    for (const header of accordionHeaders)if (header !== e.currentTarget) {
+        header.removeAttribute("active");
+        const parent = header.parentElement;
+        const content = parent.querySelector(":scope > p");
+        content.classList.add("hidden");
+    }
+    // Toggle the current item
+    e.currentTarget.toggleAttribute("active");
+    const parent = e.currentTarget.parentElement;
+    const content = parent.querySelector(":scope > p");
+    content.classList.toggle("hidden");
+}
+function handleHamburgerClick() {
+    hamburger.classList.toggle("open");
+    nav.classList.toggle("hidden");
+}
+function handleTabClick(e) {
+    const targetKey = e.target.getAttribute("key"); // The key for the tab you clicked on.
+    // Apple the styling to the selected tab.
+    for (const tab of tabs){
+        if (tab.hasAttribute("active")) tab.toggleAttribute("active");
+        if (tab.getAttribute("key") === targetKey) tab.toggleAttribute("active");
+    }
+    // Switch to the correct panel
+    for (const panel of panels)if (panel.getAttribute("key") === targetKey) panel.classList.remove("hidden");
+    else panel.classList.add("hidden");
+}
+
+//# sourceMappingURL=index.c36f364e.js.map
